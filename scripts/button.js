9c2180564213f6ce8923da7b4a05e457441b33d0
JakeOJeff <realjakeojeff@gmail.com>
2025-06-20 17:19:10 +0530

    Added Chrome Storage

--- scripts/button.js
+++ scripts/button.js
@@ -2,11 +2,14 @@ let mus1 = "assets/imgs/music.png";
 let mus2 = "assets/imgs/music2.png";
 
 document.addEventListener("DOMContentLoaded", () => {
+
+
+
   const musicButton = document.getElementById("musicButton");
   //const audio = document.getElementById("audio");
 
   musicButton.addEventListener("click", () => {
-     musicButton.src = mus2;
+    musicButton.src = mus2;
     // if (audio.paused) {
     //   //audio.play();
     //   musicButton.src = mus2;
@@ -14,5 +17,20 @@ document.addEventListener("DOMContentLoaded", () => {
     //   //audio.pause();
     //   musicButton.src = mus1;
     // }
+  });
+
+  document.getElementById("startButton").addEventListener("click", () => {
+    const name = document.getElementById("nickname").value;
+
+    chrome.storage.local.set({ savedName: name }, () => {
+      document.getElementById("signupSubtext").textContent = "Welcome, " + name + "!";
+    });
+
+
+  });
+  chrome.storage.local.get("savedName", (data) => {
+    if (data.savedName) {
+      document.getElementById("signupSubtext").textContent = "Welcome, " + data.savedName + "!";
+    }
   });
 });
